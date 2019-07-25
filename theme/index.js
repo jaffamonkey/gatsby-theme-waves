51965c69c526ea692cc5023b6a04637c083939bb
Paul Littlebury <paullittlebury@gmail.com>
2019-07-25 22:17:05 +0200

    updates

--- theme/index.js
+++
@@ -1,4 +0,0 @@
-import * as codeThemes from "code-surfer/dist/standalone.esm"
-export { default as CodeWave } from "./src/components/code-wave"
-export { default as ImageWave } from "./src/components/image-wave"
-export { codeThemes }
