9228973978df48c07b63d26123a014908647f8ba
Paul Littlebury <paullittlebury@gmail.com>
2019-07-26 19:35:19 +0200

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,23 @@
 module.exports = {
+  siteMetadata: {
+    // Used for the site title and SEO
+    title: `jaffamonkey`,
+    // Used to provide alt text for your avatar
+    author: `Paul Littlebury`,
+    // Used for SEO
+    description: `Quality`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/jaffamonkey`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/jaffamonkey`,
+      },
+    ],
+  },
   plugins: [
     "gatsby-plugin-theme-ui",
     `gatsby-plugin-sharp`,
