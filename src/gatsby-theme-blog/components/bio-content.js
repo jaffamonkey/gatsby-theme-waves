85c5e30abc2c7094d32ea7e5ea94566c3fde06e7
Paul Littlebury <paullittlebury@gmail.com>
2019-07-30 22:02:06 +0200

    updates

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -10,7 +10,6 @@ export default () => (
     <Styled.h1>Quality engineering</Styled.h1>
     <Styled.h4>A pragmatic approach to improving quality in CI & CD</Styled.h4>
     {/* <Styled.img src="./cicd.png"></Styled.img> */}
-    <Styled.div><a href="https://infallible-archimedes-b888a8.netlify.com/theclients"><img  Hspace="10" Vspace="10" src="./images/clients/nike.jpg" /><img  Hspace="10" Vspace="10" src="./images/clients/tele2.jpg" />
-<img  Hspace="10" Vspace="10" src="./images/clients/disney.jpg" /><img  Hspace="10" Vspace="10" src="./images/clients/engine.jpg" /><img  Hspace="10" Vspace="10" src="./images/clients/sonyplaystation.jpg" /></a></Styled.div>
-  </Fragment>
+    </Fragment>
 )
+  
