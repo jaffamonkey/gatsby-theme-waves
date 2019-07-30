3bf70533e727cb5456e290a08559e7143a0874fe
Paul Littlebury <paullittlebury@gmail.com>
2019-07-30 23:04:44 +0200

    updates

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -11,8 +11,8 @@ export default () => (
     <Styled.h4>A pragmatic approach to improving quality in CI & CD</Styled.h4>
     {/* <Styled.img src="./cicd.png"></Styled.img> */}
     <br/>
-    <Styled.div><a href="https://infallible-archimedes-b888a8.netlify.com/theclients"><img Hspace="5" width="40" src="./images/clients/nike.jpg" /><img Hspace="5" width="40" src="./images/clients/tele2.jpg" />
-          <img Hspace="5" width="40" src="./images/clients/disney.jpg" /><img Hspace="5" width="40" src="./images/clients/engine.jpg" /><img Hspace="5" width="40" src="./images/clients/sonyplaystation.jpg" /><img Hspace="5" width="40" src="./images/clients/citibank.jpg" /><img Hspace="5" width="40" src="./images/clients/digitas.jpg" /><img Hspace="5" width="40" src="./images/clients/incisivemedia.jpg" /></a></Styled.div>
+    <Styled.div><a href="https://infallible-archimedes-b888a8.netlify.com/theclients"><img Hspace="5" width="50" src="./images/clients/nike.jpg" /><img Hspace="5" width="50" src="./images/clients/tele2.jpg" />
+          <img Hspace="5" width="50" src="./images/clients/disney.jpg" /><img Hspace="5" width="50" src="./images/clients/engine.jpg" /><img Hspace="5" width="50" src="./images/clients/sonyplaystation.jpg" /><img Hspace="5" width="50" src="./images/clients/citibank.jpg" /><img Hspace="5" width="50" src="./images/clients/digitas.jpg" /><img Hspace="5" width="50" src="./images/clients/incisivemedia.jpg" /></a></Styled.div>
     </Fragment>
 )
   
