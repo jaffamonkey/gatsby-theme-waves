36ad6a2a0b28abded8f28bca701c1ed55bcc415a
Paul Littlebury <paullittlebury@gmail.com>
2019-07-30 03:39:57 +0200

    updates

---
+++ src/gatsby-theme-blog/components/posts.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from "react"
+import { Link } from "gatsby"
+import { Styled, css } from "theme-ui"
+
+import Layout from "../components/layout"
+import Footer from "../components/home-footer"
+
+const Posts = ({ location, posts, siteTitle, socialLinks }) => (
+  <Layout location={location} title={siteTitle}>
+    <main>
+      {posts.slice(0, 4).map(({ node }) => {
+        const title = node.title || node.slug
+        return (
+          <Fragment key={node.slug}>
+            <div>
+              <Styled.h2
+                css={css({
+                  mb: 1,
+                })}
+              >
+                <Styled.a
+                  as={Link}
+                  css={{
+                    textDecoration: `none`,
+                  }}
+                  to={node.slug}
+                >
+                  {title}
+                </Styled.a>
+              </Styled.h2>
+              <small>{node.date}</small>
+              <Styled.p>{node.excerpt}</Styled.p>
+            </div>
+          </Fragment>
+        )
+      })}
+    </main>
+    <Footer socialLinks={socialLinks} />
+  </Layout>
+)
+
+export default Posts
