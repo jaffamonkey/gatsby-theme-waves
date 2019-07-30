a0ecce319a9578189904a98f6de99ad24eafe74c
Paul Littlebury <paullittlebury@gmail.com>
2019-07-30 21:25:58 +0200

    updates

--- src/gatsby-theme-blog/components/home-footer.js
+++ src/gatsby-theme-blog/components/home-footer.js
@@ -10,21 +10,21 @@ const Footer = ({ socialLinks }) => (
   >
     {socialLinks.map((platform, i, arr) => (
       <Fragment key={platform.url}>
-        <Styled.a href={platform.url} target="_blank" rel="noopener noreferrer">
-          {platform.name}
-        </Styled.a>
-        {arr.length - 1 !== i && (
-          <Fragment>
-            {` `}&bull;{` `}
-          </Fragment>
-        )}
+<Styled.a href={platform.url} target="_blank" rel="noopener noreferrer">
+{platform.name}
+</Styled.a>
+{arr.length - 1 !== i && (
+<Fragment>
+  {` `}&bull;{` `}
+</Fragment>
+)}
       </Fragment>
     ))}
     <Fragment>
       <br /><br />
       <Styled.h1><img src="./images/code/java.svg" height="40" /><img src="./images/code/js.svg" width="40" /><img src="./images/code/php.svg" width="40" /><img src="./images/code/python.svg" width="40" /><img src="./images/code/swift.svg" width="40" /></Styled.h1>
       <br /><br />
-      <Styled.h5>© 2019 jaffamonkey B.V. (KVK 71855157) Vestigingsnr: 1012 RP Amsterdam</Styled.h5>
+      <Styled.p><strong>© 2019 jaffamonkey B.V. (KVK 71855157) Vestigingsnr: 1012 RP Amsterdam</strong></Styled.p>
     </Fragment>
   </footer>
 )
