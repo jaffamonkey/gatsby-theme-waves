e48a5aadb3eb276fda01c481a292a43a4192d5c1
Paul Littlebury <paullittlebury@gmail.com>
2019-08-09 00:41:58 +0200

    updates

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
@@ -1,10 +1,11 @@
-const purple60 = `#663399`
+const purple60 = `#191970`
 const purple30 = `#7FC4F8`
 const orange30 = `#e88200`
 const orange60 = `#e8831b`
 const grey90 = `#232129`
 const black80 = `#1B1F23`
 const white = `#fff`
+const midnightblue	 = `#696969`
 const lightWhite = `rgba(255, 255, 255, 0.86)`
 const opaqueLightYellow = `rgba(255, 229, 100, 0.2)`
 const opaqueLightWhite = `hsla(0, 0%, 100%, 0.2)`
@@ -42,7 +43,7 @@ export default {
       secondary: lightWhite,
       homeheadline: orange30,
       muted: opaqueLightWhite,
-      highlight: purple60,
+      highlight: midnightblue,
       heading: white,
     },
   },
