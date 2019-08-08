b55ec85fafc70d8daf9953e78fd49fc1cf39750c
Paul Littlebury <paullittlebury@gmail.com>
2019-08-08 16:32:10 +0200

    updates

--- src/gatsby-theme-blog/components/posts.js
+++ src/gatsby-theme-blog/components/posts.js
@@ -7,6 +7,9 @@ import Footer from "../components/home-footer"
 
 const Posts = ({ location, posts, siteTitle, socialLinks }) => (
   <Layout location={location} title={siteTitle}>
+      <Styled.div><a href="https://jaffamonkey.netlify.com/theclients"><img Hspace="5" width="45" src="./images/clients/nike.jpg" alt="Nike" /><img Hspace="5" width="45" src="./images/clients/tele2.jpg" alt="Nike" />
+  <img Hspace="5" width="45" src="./images/clients/disney.jpg" alt="Nike" /><img Hspace="5" width="45" src="./images/clients/engine.jpg" alt="Nike" /><img Hspace="5" width="45" src="./images/clients/sonyplaystation.jpg" alt="Nike" /><img Hspace="5" width="45" src="./images/clients/o2.jpg" alt="o2" /><img Hspace="5" width="45" src="./images/clients/citibank.jpg" alt="Nike" /><img Hspace="5" width="45" src="./images/clients/digitas.jpg" alt="Nike" /><img Hspace="5" width="45" src="./images/clients/incisivemedia.jpg" alt="Nike" /></a><img Hspace="5" width="45" src="./images/clients/wowcher.jpg" alt="Nike" /><img Hspace="5" width="45" src="./images/clients/bbc.jpg" alt="Nike" /><img Hspace="5" width="45" src="./images/clients/pearson.jpg" alt="Nike" />
+  <br /><br /></Styled.div>
     <main>
       {posts.slice(0, 5).map(({ node }) => {
         const title = node.title || node.slug
@@ -35,10 +38,6 @@ const Posts = ({ location, posts, siteTitle, socialLinks }) => (
         )
       })}
     </main><br />
-    <Styled.div><a href="https://jaffamonkey.netlify.com/theclients"><img Hspace="5" width="50" src="./images/clients/nike.jpg" alt="Nike" /><img Hspace="5" width="50" src="./images/clients/tele2.jpg" alt="Nike" />
-          <img Hspace="5" width="50" src="./images/clients/disney.jpg" alt="Nike" /><img Hspace="5" width="50" src="./images/clients/engine.jpg" alt="Nike" /><img Hspace="5" width="50" src="./images/clients/sonyplaystation.jpg" alt="Nike" /><img Hspace="5" width="50" src="./images/clients/citibank.jpg" alt="Nike" /><img Hspace="5" width="50" src="./images/clients/digitas.jpg" alt="Nike" /><img Hspace="5" width="50" src="./images/clients/incisivemedia.jpg" alt="Nike" /></a><img Hspace="5" width="50" src="./images/clients/wowcher.jpg" alt="Nike" /><img Hspace="5" width="50" src="./images/clients/bbc.jpg" alt="Nike" /><img Hspace="5" width="50" src="./images/clients/pearson.jpg" alt="Nike" />
-          </Styled.div>
-
     <Footer socialLinks={socialLinks} />
   </Layout>
 )
