eb2d8ca5c9cd832b8919b8a4ccdb3f744316634e
Paul Littlebury <paullittlebury@gmail.com>
2019-08-11 17:57:01 +0200

    updates

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -13,6 +13,7 @@ export default () => (
         color: `homeheadline`,
       })}
     >Quality engineering</Styled.h2>
-    <h3><code class="css-fa1w8j">Quality assurance</code> and <code class="css-fa1w8j">Test engineering</code></h3>
+    <h3><code class="inlineCode">Quality assurance and Test engineering</code></h3>
+    <Styled.div><img src="/images/testeng/wechat.svg" alt="ide" title="ide" Hspace="10" height="40" /><img src="/images/testeng/basecamp.svg" alt="pipeline" title="Software" height="40" Hspace="10" /><img src="/images/testeng/visualstudiocode.svg" alt="pipeline" title="Software" Hspace="10" height="40" /><img src="/images/testeng/github.svg" alt="pipeline" title="Software" height="40" Hspace="10" /><img src="/images/testeng/circle.svg" alt="pipeline" title="Software" height="40" Hspace="10" /><img src="/images/testeng/docker.svg" alt="pipeline" title="Software" height="40" Hspace="10" /><img src="/images/testeng/azurepipelines.svg" alt="pipeline" title="Software" height="40" Hspace="10" /></Styled.div>
   </Fragment>
 )
